68ba15e73006551a41d438cc9947d2a83e976c09
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-23 12:49:03 -0500

    installed mongoose and linked db

--- server.js
+++ server.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-
-//route files
-const bootcamps = require("./routes/bootcamps");
+const connectDB = require("./config/db.js");
 
 //load env vars
-
 dotenv.config({
   path: "./config/config.env",
 });
+
+//connect to DB
+connectDB();
+
+//route files
+const bootcamps = require("./routes/bootcamps");
 
 const app = express();
 
@@ -25,7 +28,14 @@ app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`)
+  //close server & exit process
+  server.close(() => process.exit(1));
+});
