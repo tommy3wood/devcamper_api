db34e5c9ad006eee9045adf541e9929c709af5a6
tommy3wood <thomaswcurran1014@gmail.com>
2020-04-26 16:31:14 -0500

    console colors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
+const colors = require("colors");
 const connectDB = require("./config/db.js");
 
 //load env vars
@@ -30,12 +31,14 @@ const PORT = process.env.PORT || 5000;
 
 const server = app.listen(
   PORT,
-  console.log(`server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(
+    `server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
 
 // handle unhandled promise rejections
-process.on('unhandledRejection', (err, promise) => {
-  console.log(`Error: ${err.message}`)
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
   //close server & exit process
   server.close(() => process.exit(1));
 });
